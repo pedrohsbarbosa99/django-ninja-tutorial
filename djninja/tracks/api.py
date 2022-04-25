a0be142b8993af984793b6396935cf4fb5985889
pedrohsbarbosa99 <pedrohsbarbosa99@gmail.com>
2022-04-25 20:06:32 -0300

    Added single track api

--- djninja/tracks/api.py
+++ djninja/tracks/api.py
@@ -9,3 +9,11 @@ api = NinjaAPI()
 @api.get("/tracks", response=List[TrackSchema])
 def tracks(request):
     return Track.objects.all()
+
+@api.get("/track/{track_id}", response={200: TrackSchema, 404: NotFoundSchema})
+def track(request, track_id: int):
+    try:
+        track = Track.objects.get(pk=track_id)
+        return 200, track
+    except Track.DoesNotExist as e:
+        return 404, {"message": "Track does not exist"}
